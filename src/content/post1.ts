bb2d6245e78ea2308f7feb961485f1bb46598fc6
Junior <eecfredes@gmail.com>
2024-03-03 15:10:08 -0300

    :rocket: meta dados para simular o backend

---
+++ src/content/post1.ts
@@ -0,0 +1,12 @@
+import { Post } from "../types/post";
+
+const post: Post = {
+  slug: "post1",
+  date: "2024-02-28T13:27:28Z",
+  title: "post 1",
+  description: "My first post",
+  thumbnail: "",
+  content: "",
+};
+
+export default post;
---
+++ src/content/post2.ts
@@ -0,0 +1,12 @@
+import { Post } from "../types/post";
+
+const post: Post = {
+  slug: "post2",
+  date: "2024-02-28T13:27:28Z",
+  title: "post 2",
+  description: "My second post",
+  thumbnail: "",
+  content: "",
+};
+
+export default post;
